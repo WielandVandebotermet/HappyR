b1715730c458fb8f9779d83716d4f8009e712d95
Wieland Vandebotermet <wieland.vandebotermet@inetum-realdolmen.world>
2024-03-04 09:32:14 +0100

    Base

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+﻿
+
+function Header() {
+  return (
+    <div>
+      <h1>Simple Single Page Application</h1>
+        <ul className="header">
+          
+        </ul>
+    </div>
+  );
+}
+
+function Main() {
+  return (
+    <div className="content">
+
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Main />
     </div>
   );
 }
